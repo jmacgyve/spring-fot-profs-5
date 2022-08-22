630d48aceae8db719c48bce4663bc9308e942b74
jmacgyve <eugenegoldyrev@gmail.com>
2022-08-22 23:14:10 +0300

    #Chapter4 MessageDigest factory example (add output)

--- src/main/java/com/jmacgyve/chapter4/messageDigest/Main.java
+++ src/main/java/com/jmacgyve/chapter4/messageDigest/Main.java
@@ -8,7 +8,17 @@ public class Main {
         GenericApplicationContext ctx = new AnnotationConfigApplicationContext(MessageDigestConfig.class);
         MessageDigester messageDigester = (MessageDigester) ctx.getBean("digester");
         messageDigester.digest("check");
-
         ctx.close();
     }
 }
+
+/**
+
+ digest1
+ Использую алгоритм --- SHA1
+ [B@2ddc9a9f
+ digest2
+ Использую алгоритм --- MD5
+ [B@298a5e20
+
+ */
