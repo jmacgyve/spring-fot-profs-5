0af1636c3d58f1ec64a4bffb7f04f35071d10c03
jmacgyve <eugenegoldyrev@gmail.com>
2022-08-22 23:28:57 +0300

    §

--- src/main/java/com/jmacgyve/chapter4/messageDigest/Main.java
+++ src/main/java/com/jmacgyve/chapter4/messageDigest/Main.java
@@ -1,14 +1,13 @@
 package com.jmacgyve.chapter4.messageDigest;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.GenericApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        GenericApplicationContext ctx = new AnnotationConfigApplicationContext(MessageDigestConfig.class);
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(MessageDigestConfig.class);
         MessageDigester messageDigester = (MessageDigester) ctx.getBean("digester");
         messageDigester.digest("check");
-        ctx.close();
     }
 }
 
