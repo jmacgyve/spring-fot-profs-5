cdf6a5b4ce704a6b5006f9820a5248d52a30dbcf
jmacgyve <eugenegoldyrev@gmail.com>
2022-08-26 22:06:04 +0300

    #Chapter5 Method Before Advice

---
+++ src/main/java/com/jmacgyve/chapter5/methodBeforeAdvice/Guitarist.java
@@ -0,0 +1,9 @@
+package com.jmacgyve.chapter5.methodBeforeAdvice;
+
+public class Guitarist {
+    private String song = "Trunb trunb my trunb";
+
+    public void sing(){
+        System.out.println(song);
+    }
+}
